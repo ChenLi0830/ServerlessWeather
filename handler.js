70e94da7c560c836e0ac83746074991eed9dddcc
Chen Li <yichen.li0830@gmail.com>
2016-12-04 16:01:51 -0800

    Enable Cross Origin Resource Sharing in the lambda function

--- handler.js
+++ handler.js
@@ -4,6 +4,9 @@ module.exports.currentTemperature = (event, context, callback) => {
 
   const response = {
     statusCode: 200,
+    header: {
+      'Access-Control-Allow-Origin': '*'
+    }
     body: JSON.stringify({
       temperature: 30,
       locationId: event.queryStringParameters.id,
